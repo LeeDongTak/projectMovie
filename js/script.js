340699904693e44066e14b0fa7e688295f3dd499
LeeDongTak <sunny5796@naver.com>
2023-10-21 17:52:10 +0900

    영화 리스트 검색 함수 완성

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// tmdb데이터 변수에 담기
-let movveData = []
+
 const options = {
     method: 'GET',
     headers: {
@@ -11,13 +10,47 @@ const options = {
 
 let movieList = document.querySelector("#movieList")
 let idSearch = document.querySelector("#idSearch")
-let listLi = document.querySelector("#listLi")
-let result = "";
+let searchBtn = document.querySelector("#searchBtn")
 
+let result = ""; // li의 정보를 담는 변수
 
-let printId = (id)=>{
+// 아이디를 alert으로 출력하는 함수
+let printId = (id) => {
     alert(`영화 id : ${id}`)
 }
+let searchData = []; // title의 정보를 담은 배열
+// 검색함수
+let titleSearch = () => {
+    let SearchInput = idSearch.value;
+    // 내가 검색한 제목과 리스트의 제목이 일치하는지 확인아여 
+    // 일치하는 배열의 객체를 가져온다
+    let searchFil = searchData.filter((item) => {
+        return item.title === SearchInput
+    })
+    if (searchFil[0].title === SearchInput) {
+        console.log(searchFil[0].title)
+        let titleAppend =
+            `<li class="cell" id="${searchFil[0].title}" onclick="printId(\'' + ${searchFil[0].id} + '\')">
+                    <div class="imgBox">
+                    <img src="https://image.tmdb.org/t/p/w500${searchFil[0].poster_path}" alt="">
+                    </div>
+                    <div class="movicInforBox">
+                        <span class="movieTitle">${searchFil[0].title}</span>
+                        <span class="movieoverview">${searchFil[0].overview}</span>
+                        <span class="movievote_average">평점 : ${searchFil[0].vote_average}</span>
+                    </div>
+                </li>`;
+        movieList.innerHTML = titleAppend;
+    } else {
+        alert("영화제목이 올바르지 않습니다. ");
+        return;
+    }
+
+
+}
+
+
+console.log(searchData)
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(response => response.json())
     .then(response =>
@@ -27,10 +60,9 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
             let poster_path = item.poster_path
             let overview = item.overview;
             let vote_average = item.vote_average;
-            // listLi.addEventListener("click",printId())
-            
+
             let appendHtml =
-                `<li class="cell" id="listLi" onclick="printId(\'' + ${id} + '\')">
+                `<li class="cell" onclick="printId(\'' + ${id} + '\')">
                     <div class="imgBox">
                     <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="">
                     </div>
@@ -42,6 +74,12 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
                 </li>`;
             result += appendHtml
             movieList.innerHTML = result
+
+            searchData.push(item)
+
         })
     )
     .catch(err => console.error(err));
+
+
+
